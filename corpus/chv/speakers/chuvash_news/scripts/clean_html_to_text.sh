554bd8a6bd2e2abd6a2e9ab0659101b77119bf30
Francis Tyers <ftyers@prompsit.com>
2017-11-21 12:21:03 +0100

    texts

--- corpus/chv/speakers/chuvash_news/scripts/clean_html_to_text.sh
+++ corpus/chv/speakers/chuvash_news/scripts/clean_html_to_text.sh
@@ -1,5 +1,7 @@
 for i in `cat track-id-list.txt`; do
 	python3 scripts/desoupify.py transcripts/html/$i.html > transcripts/raw/$i.txt
+	cat transcripts/raw/$i.txt | python3 scripts/sentence_segmenter.py > transcripts/txt/$i.txt
 	len=`cat transcripts/raw/$i.txt | wc -lw `
-	echo $i" "$len
+	len2=`cat transcripts/txt/$i.txt | wc -l`
+	echo $i" "$len" "$len2
 done
---
+++ corpus/chv/speakers/chuvash_news/scripts/sentence_segmenter.py
@@ -0,0 +1,31 @@
+import sys, re
+
+intxt = sys.stdin.read()
+
+abbrs = ['чӑв.']
+abbr_id = 0
+
+abbr = re.compile('([А-Я]+\.[А-Я]+\.)')
+for m in abbr.findall(intxt):
+	abbrs.append(m)
+abbr = re.compile('([А-Я]+\.)')
+for m in abbr.findall(intxt):
+	abbrs.append(m)
+
+abbrs = list(set(abbrs))
+abbr_to_id = {}
+for a in abbrs:
+	abbr_to_id[a] = '#' + str(abbr_id) + '#'
+	intxt = intxt.replace(a, abbr_to_id[a])
+	abbr_id += 1
+
+sent_id = 1
+
+for par in intxt.split('\n'):
+	lines = re.sub(r'([\.!\?]+) ', r'\1\n', par).split('\n')
+	for line in lines:
+		for k,v in abbr_to_id.items():
+			line = line.replace(v, k)
+		if line.strip() == '': continue
+		print(str(sent_id).zfill(4) + '\t' +line)
+		sent_id += 1
