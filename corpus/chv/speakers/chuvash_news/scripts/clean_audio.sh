608c3d75574fec11bee9b09a4c7bfc7d36865be9
Francis Tyers <ftyers@prompsit.com>
2017-11-22 20:34:25 +0100

    comments and debug statements

--- corpus/chv/speakers/chuvash_news/scripts/clean_audio.sh
+++ corpus/chv/speakers/chuvash_news/scripts/clean_audio.sh
@@ -2,17 +2,19 @@
 # to be run from the /audio/proc subdir
 # assuming that the original audio is in ../orig/*.flac
 
-
+echo "Cutting off silence profile..."
 # cut off seconds 1-3 (beginning) of original audio for silence profile
 for i in ../orig/*.flac; do
     sox $i begin_silence_${i##*/} trim 1 3;
 done
 
+echo "Create noise profile..."
 # create special noise profile file from silence selection
 for i in begin_silence_*; do
     sox $i -n noiseprof ${i%.flac}.prof;
 done
 
+echo "Apply noise reduction..."
 # apply noise reduction to file from its own noise silence profile
 for i in ../orig/*.flac; do
     filename=${i##*/};
@@ -20,6 +22,7 @@ for i in ../orig/*.flac; do
     sox $i clean_${filename} noisered begin_silence_${filename/%.flac}.prof 0.21;
 done
 
+echo "Trim the files..."
 # take the cleaned files and trim off the first 3 seconds
 for i in clean_*.flac; do 
 	dur=`soxi -D $i`; 
