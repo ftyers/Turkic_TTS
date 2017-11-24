914c47ebfa9e1aafde07dc191c35d7a34b557204
Francis Tyers <ftyers@prompsit.com>
2017-11-24 16:43:49 +0100

    no need to convert to wav now

--- corpus/chv/speakers/chuvash_news/scripts/segment_audio.sh
+++ corpus/chv/speakers/chuvash_news/scripts/segment_audio.sh
@@ -1,8 +1,6 @@
 for f in trim_clean_*.flac; do
 	p=`echo $f | cut -f1 -d'.'`
-	pf=$p".wav"
-	sox $f /tmp/$pf
-	for i in `../../scripts/audio_segmenter.py /tmp/$pf`; do 
+	for i in `../../scripts/audio_segmenter.py /tmp/$f`; do 
 		echo $f" "$i; 
 		id=`echo $i | cut -f1 -d','`;
 		start=`echo $i | cut -f2 -d','`;
