72cf1f373e72062d82eecefa5c5feca69126a252
Francis Tyers <ftyers@prompsit.com>
2018-01-12 15:16:51 +0100

    comments

--- corpus/chv/speakers/chuvash_news/scripts/segment_audio.sh
+++ corpus/chv/speakers/chuvash_news/scripts/segment_audio.sh
@@ -1,14 +1,20 @@
+# For each of the trimmed and cleaned files
 for f in trim_clean_*.flac; do
 	p=`echo $f | cut -f1 -d'.'`
 	ix=`echo $f | cut -f1 -d'.' | sed 's/trim_clean_//g'`
+
+	# If there is a manual segmentation then use it
 	if [[ -e ../manual/$ix.seg.txt ]]; then
 		echo -e "[m] $f"
 		cat ../manual/$ix.seg.txt > /tmp/$p.seg			
 	else
+	# Otherwise generate a new segmentation automatically
 		echo -e "[a] $f"
 		python3 ../../scripts/audio_segmenter.py $f > /tmp/$p.seg
 	fi
 
+	# Run through each line in the segmentation and use
+	# sox to split the file according to the start/end points
 	for i in `cat /tmp/$p.seg`; do 
 		echo $f" "$i; 
 		id=`echo $i | cut -f1 -d','`;
